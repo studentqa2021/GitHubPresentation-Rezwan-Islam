0d1d4f428893261b825fe6a6a5736581b359bfa4
sarowerhome@gmail.com <sarowerhome@gmail.com>
2021-09-02 22:27:28 -0400

    Batch14 class added to a new package named com.practice.github

---
+++ src/main/java/com/practice/github/Batch14.java
@@ -0,0 +1,11 @@
+package com.practice.github;
+
+public class Batch14 {
+	public void getJob() {
+		System.out.println("Best of luck to all students of Batch 14 on their job searching!");
+	}
+	
+	public static void main(String[] args) {
+		new Batch14().getJob();
+	}
+}
